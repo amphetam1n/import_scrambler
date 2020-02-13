7794bcabea87ead8e0556428e3adaa7a910c3801
amphetam1n <amphetam1n@protonmail.ch>
2020-02-13 19:32:51 +0100

    im stupid :sunglasses:

--- example/main.cpp
+++ example/main.cpp
@@ -4,6 +4,6 @@ int main( int argc, char** argv )
 {
 	LoadLibraryA( "user32.dll" );
 	typedef int( __stdcall* MessageBoxA )( HWND, LPCSTR, LPCSTR, UINT );
-	AMPHETAM1N_IMPORT( MessageBoxA )( NULL, "amphetam1n/import_scrambler", "Hello, world!", MB_OK );
+	AMPHETAM1N_IMPORT( MessageBoxA )( NULL, "Hello, world!", "amphetam1n/import_scrambler", MB_OK );
 	return 0;
 }
